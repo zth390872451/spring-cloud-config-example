ac163dfc1e8ace2f1d37adf9fd4b620df7e3378f
zth390872451 <zth390872451@gmail.com>
2016-11-14 18:18:00 +0800

    Feign 作为消费的客户端

---
+++ eureka-feign-consumer/src/main/java/com/company/FeignComsumerApplication.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.gizwits.controller","com.gizwits.service"})
+@ComponentScan(value = "com.company")
+public class FeignComsumerApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(FeignComsumerApplication.class);
+
+
+    public static void main(String[] args) throws Exception {
+
+        //开启监控 http://localhost:8080/health
+
+        SpringApplication.run(FeignComsumerApplication.class, args);
+
+    }
+}
