728f0e77e8fb31df64597044aa8fb58fb05cdd87
Administrator <zth390872451@gmail.com>
2016-11-23 01:29:28 +0800

    健康监测模块应用

---
+++ spring-boot-starter-actuator-demo/src/main/java/com/web/HealthMonitorApplication.java
@@ -0,0 +1,22 @@
+package com.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 健康监测中心
+ */
+@SpringBootApplication
+public class HealthMonitorApplication {
+
+    public static final Map<String,String> params = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HealthMonitorApplication.class).web(true).run(args);
+
+    }
+
+}
