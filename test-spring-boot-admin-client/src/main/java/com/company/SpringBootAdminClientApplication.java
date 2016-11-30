3994b130a72a27fe986cb6c8bc220f85f6b8c71f
zth390872451 <zth390872451@gmail.com>
2016-11-30 18:35:30 +0800

    Spring-boot-admin 的客户端和服务端

---
+++ test-spring-boot-admin-client/src/main/java/com/company/SpringBootAdminClientApplication.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2016/11/30.
+ */
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
---
+++ test-spring-boot-admin/src/main/java/com/company/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2016/11/30.
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
